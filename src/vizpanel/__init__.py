d8206daf02053da4a8db10f0bce8d7b291cf1b5f
randomseed42 <randomseed42@protonmail.com>
2025-05-23 08:26:44 +0800

    init

---
+++ src/vizpanel/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '0.1.0'
+
+from .vizpanel import VizPanel
+
+__all__ = [
+    'VizPanel',
+]
---
+++ src/vizpanel/main.py
@@ -0,0 +1,10 @@
+from .vizpanel import VizPanel
+
+
+def main():
+    vp = VizPanel()
+    vp.run()
+
+
+if __name__ == '__main__':
+    main()
